0c49b74ccd7e03a6b8a4df7984d91805232d2ae5
senpai <christianseremetis@gmail.com>
2018-07-17 07:59:31 -0400

    nested base class from BasicGraph

--- lib/Graph.rb
+++ lib/Graph.rb
@@ -3,40 +3,131 @@
 #8 April 2018
 #An outline for all forms of graphs
 module Graph
-	attr_reader :nodes
+	class Base
+		QUEUE = []
 
-	#Returns the node with the given data.
-	#@param data the data stored in the node
-	def gatherNode(data)
-	end
+		#Public constructor initializes nodes array.
+		def initialize
+			@nodes = []
+		end
 
-	#Add an edge to the graph.
-	#@param start the starting node
-	#@param finish the ending node 
-	def insertEdge(start, finish)
-	end
+		#Returns the node with the given data.
+		#@param data the data stored in the node
+		def gatherNode(data)
+			@nodes.bsearch { |n| n.data == data}
+		end
 
-	#Adds a node to the graph.
-	#nodes can be stand-alone and do not require an edge
-	#@param data the data stored in the node
-	def insertNode(data)
-	end
+		#Add an edge to the graph.
+		#@param start the starting node
+		#@param finish the ending node 
+		def insertEdge(start, finish)
+			if start == finish || edge?(start, finish)
+				raise "Invalid Edge input"
+			end
+			
+			start.to << finish
+			finish.from << start
+		end
 
-	#Prints a string representation of the graph
-	def print
-	end
+		#Adds a node to the graph.
+		#nodes can be stand-alone and do not require an edge
+		#@param data the data stored in the node
+		def insertNode(data)
+			node = Node.new(data)
+			@nodes << node
+			return node
+		end
 
-	#Searches over the nodes by breadth
-	#@param root the starting node
-	#@param subject the node we're looking for
-	#@return the shortest path between nodes
-	def breadthFirstSearch(root, subject)
-	end
+		#Prints a string representation of the graph
+		def toString
+			result = ""
+			@nodes.each do |n|
+				n.to.each do |t|
+					result += "#{n.data} -> #{t.data}\n"
+				end
+			end
 
-	#Only applies to subgraphs of connected nodes
-	#@param root data stored in the starting node
-	#@param subject what we're searching for
-	def depthFirstSearch(root, subject)
+			print result
+		end
+
+		#Searches over the nodes by breadth
+		#@param root the starting node
+		#@param subject the node we're looking for
+		#@return the shortest path between nodes
+		def breadthFirstSearch(root, subject)
+			#the shortest path between the two nodes
+			path = []
+			QUEUE.insert(0, root)
+
+			while QUEUE.any?
+				node = QUEUE.pop
+				node.to.each do |t|
+					t.pathMarker = node
+
+					if t == subject
+						return true
+					end
+
+					QUEUE.insert(0, t)
+				end
+			end
+
+			return false
+		end
+
+		#Only applies to subgraphs of connected nodes
+		#DFS Implementation here starts from root,
+		#adds all outgoing vertices into the QUEUE
+		#and runs recursively until we find the subject
+		def depthFirstSearch(root, subject)
+			if root == subject
+				return root
+			elsif !root.to.any?
+				return root
+			else
+				root.to.each do |n|
+				 	QUEUE.insert(0, n)
+				end
+			end
+
+			loop do
+				node = QUEUE.pop
+				root.pathMarker = node
+				@tester = depthFirstSearch(node, subject)
+				break if @tester == subject
+			end
+
+			return @tester
+		end
+
+		#Nested Node class
+		#Nodes store three instance variables
+		#to: an array of adjacent nodes from which the given one points
+		#from: an array of adjacent nodes pointing towards the given one
+		#data: the data stored in the node
+		class Node
+			attr_reader :to, :from, :data
+			attr_accessor :pathMarker
+
+			#Public constructor.
+			#@param data the data stored in the node
+			def initialize(data)
+				@to = []
+				@from = []
+				@data = data
+			end
+		end
 		
+
+		private
+			#Checks to see if an edge exists between two nodes
+			def edge?(start, finish)
+				if start.to.include? finish
+					true
+				else
+					false
+				end
+			end
+
 	end
 end
