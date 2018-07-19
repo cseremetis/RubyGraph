11cc2b0b8720697b24ba752929ed1e82db88e5ba
senpai <christianseremetis@gmail.com>
2018-07-19 18:57:57 -0400

    changed to NeuralNetwork.rb

--- lib/bipartite.rb
+++ lib/bipartite.rb
@@ -1,4 +1,25 @@
+require_relative './Base.rb'
+
 module Graph
-	class Bipartite
+	#A graph implementation in which nodes can be broken
+	#up into 2 sets
+	class Bipartite < Graph::Base
+
+		#Nodes are split into "red" and "black" colors
+		def initialize
+			@red = []
+			@black = []
+		end
+
+		#Determines if the graph is cyclic
+		def cycle?
+			#...
+		end
+
 	end
-end	
+
+	class Graph::Base::Node
+		#Color determines which of 2 sets a node belongs to
+		attr_accessor :color
+	end
+end
